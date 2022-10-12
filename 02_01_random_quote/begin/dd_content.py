bd1a7a561f1f292cc74c35da32f42ec225aefff5
Eddynah Neymour <111374892+EddynahNeymour@users.noreply.github.com>
2022-10-12 08:49:54 -0700

    Add files via upload

---
+++ 02_01_random_quote/begin/dd_content.py
@@ -0,0 +1,14 @@
+def get_random_quote():
+    pass
+
+def get_sale_events():
+    pass
+
+def get_fashion_article():
+    pass
+
+def get_twitter_trends():
+    pass
+
+if __name__=='_main_':
+    pass #test code
